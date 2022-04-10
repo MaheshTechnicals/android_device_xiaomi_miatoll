7b3513c78470f3f58254c74bd1dd200783194349
MaheshTechnicals <msvarma10001@gmail.com>
2022-04-10 03:23:58 +0000

    gapps suport

--- dot_miatoll.mk
+++ dot_miatoll.mk
@@ -26,6 +26,16 @@ PRODUCT_MANUFACTURER := Xiaomi
 
 PRODUCT_GMS_CLIENTID_BASE := android-xiaomi
 
+# Set this flag in build script
+ifeq ($(CURRENT_BUILD_TYPE), gapps)
+# Use Gapps
+TARGET_SHIPS_SEPERATE_GAPPS_BUILD := true
+WITH_GAPPS := true
+TARGET_GAPPS_ARCH := arm64
+IS_PHONE := true
+endif
+
+
 # Maintainer
 DEVICE_MAINTAINER := MaheshTechnicals
 
